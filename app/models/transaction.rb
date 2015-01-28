37f191c2d61171d2db902528c6db1b8546e451ef
Thomas Midgett <tmidge20@gmail.com>
2015-01-28 15:42:27 -0500

    I do not like scaffolding

---
+++ app/models/transaction.rb
@@ -0,0 +1,13 @@
+class Transaction < ActiveRecord::Base
+  def self.balance
+    balance = 0
+    deposit_array = []
+    array = Transaction.all.each do |transaction|
+      deposit_array << transaction.deposit
+    end
+    balance = deposit_array.reduce(:+)
+    return balance
+  end
+
+
+end
